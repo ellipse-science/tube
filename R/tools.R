eb73a295459236d43e07106ef402d9606634005f
p2xcode <p2x@p2xco.de>
2023-06-26 21:57:53 +0000

    added dummy function

---
+++ R/tools.R
@@ -0,0 +1,14 @@
+#' display the package version
+#'
+#' @param none
+#'
+#' @examples
+#' \dontrun{
+#'   v <- version()
+#'   print(v)
+#' }
+#'
+#' @export
+version <- function() {
+  return(0.0.1)
+}
