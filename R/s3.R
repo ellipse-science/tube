d7a489ee22ab281cc4b218e51d3660602ea6b573
p2xcode <p2x@p2xco.de>
2023-06-27 04:10:51 +0000

    addind dummy s3 datalake  commit function

---
+++ R/s3.R
@@ -0,0 +1,14 @@
+#' commits a dataframe to an S3 object
+#' @param df
+#' @param metadata
+#' @param bucket
+#'
+#' @examples
+#' \dontrun{
+#'   # pute some sample code here as an example
+#' }
+#'
+#' @export
+commit_df_to_datalake <- function(df, metadata, bucket) {
+  logger::log_info("committing df to datalake")
+}
