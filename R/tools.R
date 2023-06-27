f25abca7a85a8f68ceab6677f1fecdebc40bacc1
p2xcode <p2x@p2xco.de>
2023-06-27 19:25:50 +0000

    added convert_url_to_key

--- R/tools.R
+++ R/tools.R
@@ -12,3 +12,21 @@
 version <- function() {
   return("0.0.1")
 }
+
+
+#' convert a url into a string
+#'
+#' @param url
+#'
+#' @examples
+#' \dontrun{
+#'   r <- convert_url_to_key(url)
+#'   print(r)
+#' }
+#'
+#' @export
+convert_url_to_key <- function(url) {
+  r <- gsub(" |-|:|/|\\.|&|\\?|=", "_", paste(format(Sys.time(), "%Y-%m-%d-%H-%M"), url, sep="_"))
+  r <- gsub("https?___", "", r)
+  return(r)
+}
