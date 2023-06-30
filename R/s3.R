d0b6b071b951e85ae4cfa7f0f5f52b1f100bac93
p2xcode <p2x@p2xco.de>
2023-06-30 17:15:43 +0000

    replace s3 communications with paws.storage package

--- R/s3.R
+++ R/s3.R
@@ -13,7 +13,7 @@
 #' }
 #'
 #' @export
-commit_r_object_to_datalake <- function(object, metadata, objectname, path, object_ext, bucket, refresh_data) {
+commit_r_object_to_datalake <- function(aws_client, object, tags, objectname, path, object_ext, bucket, refresh_data) {
   logger::log_debug("[pump::commit_r_object_to_datalake] entering function")
   logger::log_info("[pump::commit_r_object_to_datalake] committing object to datalake")
 
@@ -25,14 +25,27 @@ commit_r_object_to_datalake <- function(object, metadata, objectname, path, obje
   # todo : detect object type (df, list, character etc) and write accordingly
   write(object, file.path(td, filename))
 
-  names(metadata) <- paste("x-amz-meta-", names(metadata), sep = "")
-  
-  aws.s3::put_object(
-    file = file.path(td, filename), 
-    object = paste(path,objectname,sep="/"),
-    bucket = bucket,
-    headers = metadata
+  #names(metadata) <- paste("x-amz-meta-", names(metadata), sep = "")
+  aws_client$put_object(
+    Bucket = bucket,
+    Body = file.path(td, filename),
+    Key = paste(path,objectname,sep="/"),
+    Tagging = paste(tags, names(tags), collapse="&", sep="=")
   )
+
+  aws_client$put_object_tagging(
+
+  )
+  
+
+  # aws.s3::put_object(
+  #   file = file.path(td, filename), 
+  #   object = paste(path,objectname,sep="/"),
+  #   bucket = bucket,
+  #   headers = metadata
+  # )
+
+  
 
   logger::log_debug("[pump::commit_r_object_to_datalake] exiting function")
 }
@@ -56,7 +69,7 @@ commit_r_object_to_datalake <- function(object, metadata, objectname, path, obje
 #' @export
 get_datalake_inventory <- function(bucket, path, metadata_filter) {
   logger::log_debug("[pump::get_datalake_inventory] entering function")
-  logger::log_info("[pump::get_datalake_inventory] committing object to datalake")
+  logger::log_info("[pump::get_datalake_inventory] listing object from datalake")
 
   # TODO: checkmate parameters validations and error handling
   
@@ -64,9 +77,11 @@ get_datalake_inventory <- function(bucket, path, metadata_filter) {
   
   r <- aws.s3::get_bucket(
     bucket = bucket,
-    path = path,
+    prefix = paste(path, "/", sep=""),
+    delimiter = "/",
     headers =  metadata_filter
   )
+
 
   logger::log_debug("[pump::commit_r_object_to_datalake] exiting function")
   return(r)
