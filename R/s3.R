fa94ee529f874aad3bfecd57a6c098a2fcd29b19
p2xcode <p2x@p2xco.de>
2023-07-01 01:37:58 +0000

    implement objects filter based on tags

--- R/s3.R
+++ R/s3.R
@@ -33,14 +33,14 @@ commit_r_object_to_datalake <- function(aws_client, bucket, object, tags, object
     Tagging = URLencode(paste(names(tags), tags, collapse="&", sep="="))
   )  
 
+  #TODO : Error management
+
   # aws.s3::put_object(
   #   file = file.path(td, filename), 
   #   object = paste(path,objectname,sep="/"),
   #   bucket = bucket,
   #   headers = metadata
   # )
-
-  
 
   logger::log_debug("[pump::commit_r_object_to_datalake] exiting function")
 }
@@ -62,20 +62,40 @@ commit_r_object_to_datalake <- function(aws_client, bucket, object, tags, object
 #' }
 #'
 #' @export
-get_datalake_inventory <- function(bucket, path, metadata_filter) {
+get_datalake_inventory <- function(bucket, path, tags_filter) {
   logger::log_debug("[pump::get_datalake_inventory] entering function")
   logger::log_info("[pump::get_datalake_inventory] listing object from datalake")
 
   # TODO: checkmate parameters validations and error handling
   
-  names(metadata_filter) <- paste("x-amz-meta-", names(metadata_filter), sep = "")
-  
-  r <- aws.s3::get_bucket(
-    bucket = bucket,
-    prefix = paste(path, "/", sep=""),
-    delimiter = "/",
-    headers =  metadata_filter
-  )
+  #names(metadata_filter) <- paste("x-amz-meta-", names(metadata_filter), sep = "")
+
+  r <- s3_client$list_objects_v2(
+    Bucket = bucket, 
+    Prefix = paste(path, "/", sep=""),
+    Delimiter = "/")
+
+  r$Contents[[1]] <- NULL
+
+  for (o in r$Contents) {
+    t <- s3_client$get_object_tagging(datalake_bucket, o$Key)
+    t$VersionId <- NULL
+    df <- as.data.frame(sapply((sapply(t, unname)), unname))
+    names(df) <- unlist(df[1,])
+    df <- df[-c(1),]
+    l1 <- lapply(as.list(df), unlist)
+    int <- intersect(names(l1), names(tags_filter))
+    l2 <- l1[int]
+    if (FALSE %in% (l2 %in% tags_filter == names(l2) %in% names(tags_filters))) next
+    l[length(l)+1] <-list(l1)
+  }
+
+  # r <- aws.s3::get_bucket(
+  #   bucket = bucket,
+  #   prefix = paste(path, "/", sep=""),
+  #   delimiter = "/",
+  #   headers =  metadata_filter
+  # )
 
 
   logger::log_debug("[pump::commit_r_object_to_datalake] exiting function")
