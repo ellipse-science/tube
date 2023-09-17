043caf408faa2382df0bc5f7b6f9c4c6d2b6f22d
p2xcode <p2x@p2xco.de>
2023-09-17 01:08:31 +0000

    added get dw_table

--- R/datawarehouse.R
+++ R/datawarehouse.R
@@ -4,8 +4,97 @@ list_datawarehouses <- function() {
 }
 
 #' @export
-get_datawarehouse_table <- function() {
+get_datawarehouse_table <- function(credentials, datawarehouse_name, table_name, columns = NULL, filter = NULL) {
+  logger::log_debug("[pumpr::get_datawarehouse_content] entering function")
 
+  datawarehouse_name <- strsplit(data_source, "\\.")[[1]][1]
+  database_name <- strsplit(data_source, "\\.")[[1]][2]
+  table_name <- strsplit(data_source, "\\.")[[1]][3]
+
+  # TODO: checkmate parameters validations and error handling
+
+  if (pipeline_handler == "lambda") {
+    con <- DBI::dbConnect(
+      noctua::athena(),
+      s3_staging_dir=paste("s3:/", datawarehouse_name, table_name, sep="/"),
+      region_name='ca-central-1'
+    )
+  } else {
+    con <- DBI::dbConnect(
+      noctua::athena(),
+      aws_access_key_id=Sys.getenv("AWS_ACCESS_KEY_ID"),
+      aws_secret_access_key=Sys.getenv("AWS_SECRET_ACCESS_KEY"),
+      s3_staging_dir=paste("s3:/", datawarehouse_name, table_name, sep="/"),
+      region_name='ca-central-1'
+    )
+  }
+
+  #logger::log_debug("[pumpr::get_datawarehouse_content] listing tables")
+  #DBI::dbListTables(con)
+
+  logger::log_debug("[pumpr::get_datawarehouse_content] executing query")
+
+  columns_string <- if (typeof(columns) == "list") paste(columns, collapse = ",")
+  filter_string <- if (!is.null(unlist(filter))) {
+    paste(
+      "WHERE ",
+      trimws(
+        paste(
+          if (length(filter$metadata)) paste(paste(names(filter$metadata), paste("'", filter$metadata, "'", sep=""), sep="=", collapse=" AND ")) else "",
+          if (length(filter$metadata) && length(filter$data) > 0) "AND" else "",
+          if (length(filter$data)) paste(paste(names(filter$data), paste("'", filter$data, "'", sep=""), sep="=", collapse=" AND ")) else "",
+          sep = " "
+        )
+      )
+    )
+  } else {
+    ""
+  }
+
+  query <- paste(
+    "SELECT ", columns_string, " FROM \"", database_name, "\".\"",
+    table_name, "\"", filter_string, ";", sep = "")
+
+  logger::log_debug(paste("[pumpr::get_datalake_content] query string is", query))
+  
+  res <- NULL
+
+  res <- tryCatch(
+    expr = { DBI::dbExecute(con, query) },
+    error = function(e) {
+      if (grepl("TABLE_NOT_FOUND", e$message)) {
+        msg <- paste(
+          "[pumpr::get_datawarehouse_content] The table specified",
+          data_source,
+          "does not exist...  the dataframe returned is NULL"
+        )
+        logger::log_error(msg)
+      } else {
+        msg <- paste(
+          "[pumpr::get_datawarehouse_content] an error occurred: ",
+          e$message
+        )
+        logger::log_error(msg)
+      }
+      return(NULL)
+    },
+    finally = {}
+  )
+
+  if (!is.null(res)) {
+    df <- DBI::dbFetch(res)
+    DBI::dbClearResult(res)
+
+    if (nrow(df) == 0) {
+      logger::log_warn("[pumpr::get_datawarehouse_content] The query was successful but the dataframe returned is empty.  Check the columns or the filter you sent to the function")
+    }
+  } else {
+    logger::log_debug("[pumpr::get_datalake_content] setting null dataframe")
+    df <- NULL  
+  }
+
+  logger::log_debug("[pumpr::get_datawarehouse_content] exiting function")
+  return(df)
 }
 
 #' @export
@@ -15,7 +104,6 @@ put_datawarehouse_table <- function() {
 
 #' @export 
 update_datawarehouse_table <- function() {
-
 }
 
 #' @export 
--- R/s3.R
+++ R/s3.R
@@ -1,107 +1,3 @@
-#' commits an R object to an S3 bucket
-#' @param PARM1
-#' @param PARM2
-#' @param PARM3
-#' @param PARM4
-#'
-#' @examples
-#' \dontrun{
-#'   # put some sample code here as an example
-#' }
-#'
-#' @export
-commit_r_object_to_datalake <- function(
-                                    s3_client,
-                                    bucket, 
-                                    key, 
-                                    metadata, 
-                                    data, 
-                                    prefix, 
-                                    keep_history, 
-                                    history_schema, 
-                                    refresh_data) {
-
-  logger::log_debug("[pumpr::commit_r_object_to_datalake] entering function")
-  logger::log_info("[pumpr::commit_r_object_to_datalake] committing object to datalake")
-
-  # TODO: checkmate parameters validations and error handling
-  checkmate::assertChoice(
-    tolower(metadata$content_type), 
-    tolower(c(
-      "text/html; charset=utf-8",
-      "application/rss\\+xml",
-      "application/json"
-    ))
-  )
-  
-  # figure our path (s3 prefix) based on whether we have to keep_history or not
-  if (keep_history) {
-    # we'll handle the history_granularity later
-    # for now if keey history, then we partition the path based on partition schema
-    # Partition schemas:
-    # YYYY
-    # YYYY/MM
-    # YYYY/MM/DD
-    # YYYY/MM/DD/HH
-    # YYYY/WEEKNUM 
-
-    # compute the partition prefix
-    partition_suffix <- dplyr::case_when(
-      history_schema == "YYYY" ~ format(Sys.Date(), format="%Y"),
-      history_schema == "YYYY/MM" ~ format(Sys.Date(), format="%Y/%m"),
-      history_schema == "YYYY/MM/DD" ~ format(Sys.Date(), format="%Y/%m/%d"),
-      history_schema == "YYYY/MM/DD/HH" ~ format(Sys.time(), format="%Y/%m/%d/%H"),
-      history_schema == "YYYY/WEEKNUM" ~ format(Sys.time(), format="%Y/%W"),      
-    )
-
-    prefix <- paste(prefix, partition_suffix, sep="/")
-
-    metadata$partitionned = "TRUE"
-    metadata$partition_schema = history_schema
-    metadata$partition = partition_suffix
-  } else {
-    metadata$partitionned = "FALSE"
-    metadata$partition_schema <- NA_character_
-    metadata$partition <- NA_character_
-  }
-
-  #split_metadata <- paste(paste("metadata", names(metadata), sep="."), metadata, collapse = ",", sep = ":")
-  #ist(split_metadata)
-
-  names(metadata) <- paste("metadata", names(metadata), sep = "_")
-
-  # we're in lambda so we'll use a temporary fildsystem
-  td <- tempdir()
-  filename <- paste(key, "json", sep = ".")
-
-  # build json object
-  json_object <- jsonlite::toJSON(
-    c(
-      #key = gsub(".json$", "", paste(base_path,filename,sep="/")),
-      key = gsub(".json$", "", filename),
-      metadata,
-      data
-    ),
-    auto_unbox = T
-  )
-
-  # todo : detect object type (df, list, character etc) and write accordingly
-  write(json_object, file.path(td, filename))
-
-  # put the object in s3 bucket 
-  s3_client$put_object(
-    Bucket = bucket,
-    Body = file.path(td, filename),
-    Key = paste(prefix, filename, sep="/"),
-    ContentType = "application/json; charset=utf-8"
-  )  
-
-  #TODO : Error management
-
-  logger::log_debug("[pumpr::commit_r_object_to_datalake] exiting function")
-}
-
-
 
 
 
@@ -156,144 +52,6 @@ get_r_object_from_datalake <- function(
 
 
 
-#' retrieves a list and returns a dataframe of R objects from an S3 bucket through GLUE
-#' @param PARM1
-#' @param PARM2
-#' @param PARM3
-#' @param PARM4
-#'
-#' @examples
-#' \dontrun{
-#'   # put some sample code here as an example
-#' }
-#'
-#' @export
-get_datalake_content <- function(
-                              data_source, 
-                              columns = list(), 
-                              filter = list(),
-                              download_data = FALSE,
-                              pipeline_handler = "lambda") {
-
-  logger::log_debug("[pumpr::get_datalake_content] entering function")
-
-  datalake_name <- strsplit(data_source, "\\.")[[1]][1]
-  database_name <- strsplit(data_source, "\\.")[[1]][2]
-  table_name <- strsplit(data_source, "\\.")[[1]][3]
-
-  # TODO: checkmate parameters validations and error handling
-
-  if (pipeline_handler == "lambda") {
-    con <- DBI::dbConnect(
-      noctua::athena(),
-      s3_staging_dir=paste("s3:/", datalake_name, table_name, sep="/"),
-      region_name='ca-central-1'
-    )
-  } else {
-    con <- DBI::dbConnect(
-      noctua::athena(),
-      aws_access_key_id=Sys.getenv("AWS_ACCESS_KEY_ID"),
-      aws_secret_access_key=Sys.getenv("AWS_SECRET_ACCESS_KEY"),
-      s3_staging_dir=paste("s3:/", datalake_name, table_name, sep="/"),
-      region_name='ca-central-1'
-    )
-  }
-
-  #logger::log_debug("[pumpr::get_datalake_content] listing tables")
-  #DBI::dbListTables(con)
-
-  logger::log_debug("[pumpr::get_datalake_content] executing query")
-
-  columns_string <- if (typeof(columns) == "list") paste(columns, collapse = ",")
-  filter_string <- if (!is.null(unlist(filter))) {
-    paste(
-      "WHERE ",
-      trimws(
-        paste(
-          if (length(filter$metadata)) paste(paste(names(filter$metadata), paste("'", filter$metadata, "'", sep=""), sep="=", collapse=" AND ")) else "",
-          if (length(filter$metadata) && length(filter$data) > 0) "AND" else "",
-          if (length(filter$data)) paste(paste(names(filter$data), paste("'", filter$data, "'", sep=""), sep="=", collapse=" AND ")) else "",
-          sep = " "
-        )
-      )
-    )
-  } else {
-    ""
-  }
-
-  logger::log_debug( 
-    paste(
-      "[pumpr::get_datalake_content] query string is",
-      paste(
-        "SELECT ", 
-        columns_string,
-        " FROM \"",
-        database_name,
-        "\".\"",
-        table_name,
-        filter_string,
-        ";",
-        sep = ""
-      )
-    )
-  )
-  res <- NULL
-
-  res <- res <- tryCatch(
-    expr = {
-      DBI::dbExecute(
-        con, 
-        paste(
-          "SELECT ", 
-          columns_string,
-          " FROM \"",
-          database_name,
-          "\".\"",
-          table_name,
-          "\"",
-          filter_string,
-          ";",
-          sep = ""
-        )
-      )
-    },
-    error = function(e) {
-      if (grepl("TABLE_NOT_FOUND", e$message)) {
-        msg <- paste(
-          "[pumpr::get_datawarehouse_content]",
-          "The table specified",
-          data_source,
-          "does not exist...  the dataframe returned is NULL"
-        )
-        logger::log_error(msg)
-      } else {
-        msg <- paste(
-          "[pumpr::get_datawarehouse_content]",
-          "an error occurred",
-          e$message
-        )
-        logger::log_error(msg)
-      }
-      return(NULL)
-    },
-    finally = {}
-  )
-
-  if (!is.null(res)) {
-    df <- DBI::dbFetch(res)
-    DBI::dbClearResult(res)
-
-    if (nrow(df) == 0) {
-      logger::log_warn("[pumpr::get_datalake_content] The query was successful but the dataframe returned is empty.  Check the columns or the filter you sent to the function")
-    }
-  } else {
-    logger::log_debug("[pumpr::get_datalake_content] setting null dataframe")
-    df <- NULL
-  }
-
-  logger::log_debug("[pumpr::get_datalake_content] exiting function")
-  return(df)
-}
 
 
 
@@ -309,132 +67,7 @@ get_datalake_content <- function(
 #' }
 #'
 #' @export
-get_datawarehouse_content <- function(
-                                  data_source, 
-                                  columns = list(), 
-                                  filter = list(),
-                                  download_data = FALSE,
-                                  pipeline_handler = "lambda") {
 
-  logger::log_debug("[pumpr::get_datawarehouse_content] entering function")
-
-  datawarehouse_name <- strsplit(data_source, "\\.")[[1]][1]
-  database_name <- strsplit(data_source, "\\.")[[1]][2]
-  table_name <- strsplit(data_source, "\\.")[[1]][3]
-
-  # TODO: checkmate parameters validations and error handling
-
-  if (pipeline_handler == "lambda") {
-    con <- DBI::dbConnect(
-      noctua::athena(),
-      s3_staging_dir=paste("s3:/", datawarehouse_name, table_name, sep="/"),
-      region_name='ca-central-1'
-    )
-  } else {
-    con <- DBI::dbConnect(
-      noctua::athena(),
-      aws_access_key_id=Sys.getenv("AWS_ACCESS_KEY_ID"),
-      aws_secret_access_key=Sys.getenv("AWS_SECRET_ACCESS_KEY"),
-      s3_staging_dir=paste("s3:/", datawarehouse_name, table_name, sep="/"),
-      region_name='ca-central-1'
-    )
-  }
-
-  #logger::log_debug("[pumpr::get_datawarehouse_content] listing tables")
-  #DBI::dbListTables(con)
-
-  logger::log_debug("[pumpr::get_datawarehouse_content] executing query")
-
-  columns_string <- if (typeof(columns) == "list") paste(columns, collapse = ",")
-  filter_string <- if (!is.null(unlist(filter))) {
-    paste(
-      "WHERE ",
-      trimws(
-        paste(
-          if (length(filter$metadata)) paste(paste(names(filter$metadata), paste("'", filter$metadata, "'", sep=""), sep="=", collapse=" AND ")) else "",
-          if (length(filter$metadata) && length(filter$data) > 0) "AND" else "",
-          if (length(filter$data)) paste(paste(names(filter$data), paste("'", filter$data, "'", sep=""), sep="=", collapse=" AND ")) else "",
-          sep = " "
-        )
-      )
-    )
-  } else {
-    ""
-  }
-
-  logger::log_debug( 
-    paste(
-      "[pumpr::get_datalake_content] query string is",
-      paste(
-        "SELECT ", 
-        columns_string,
-        " FROM \"",
-        database_name,
-        "\".\"",
-        table_name,
-        filter_string,
-        ";",
-        sep = ""
-      )
-    )
-  )
-  res <- NULL
-
-  res <- tryCatch(
-    expr = {
-      DBI::dbExecute(
-        con, 
-        paste(
-          "SELECT ", 
-          columns_string,
-          " FROM \"",
-          database_name,
-          "\".\"",
-          table_name,
-          "\"",
-          filter_string,
-          ";",
-          sep = ""
-        )
-      )
-    },
-    error = function(e) {
-      if (grepl("TABLE_NOT_FOUND", e$message)) {
-        msg <- paste(
-          "[pumpr::get_datawarehouse_content]",
-          "The table specified",
-          data_source,
-          "does not exist...  the dataframe returned is NULL"
-        )
-        logger::log_error(msg)
-      } else {
-        msg <- paste(
-          "[pumpr::get_datawarehouse_content]",
-          "an error occurred",
-          e$message
-        )
-        logger::log_error(msg)
-      }
-      return(NULL)
-    },
-    finally = {}
-  )
-
-  if (!is.null(res)) {
-    df <- DBI::dbFetch(res)
-    DBI::dbClearResult(res)
-
-    if (nrow(df) == 0) {
-      logger::log_warn("[pumpr::get_datawarehouse_content] The query was successful but the dataframe returned is empty.  Check the columns or the filter you sent to the function")
-    }
-  } else {
-    logger::log_debug("[pumpr::get_datalake_content] setting null dataframe")
-    df <- NULL  
-  }
-
-  logger::log_debug("[pumpr::get_datawarehouse_content] exiting function")
-  return(df)
-}
 
 
 #' retrieves a list and returns a dataframe of R objects from an S3 bucket through GLUE
@@ -464,9 +97,7 @@ commit_dataframe_to_datawarehouse <- function(
     } else {
       s3_client <<- paws.storage::s3(
         config = list(
-          credentials = list(
-            creds = datawarehouse$credentials
-          ),
+          credentials = list(creds = datawarehouse$credentials),
           region = Sys.getenv("AWS_REGION")
         )
       )
@@ -482,6 +113,53 @@ commit_dataframe_to_datawarehouse <- function(
       Body = filename,
       Key = paste(prefix, paste(key, ".parquet", sep=""), sep="/")
     )  
+  }
+
+}
+
+
+
+#' retrieves a list and returns a dataframe of R objects from an S3 bucket through GLUE
+#' @param PARM1
+#' @param PARM2
+#' @param PARM3
+#' @param PARM4
+#'
+#' @examples
+#' \dontrun{
+#'   # put some sample code here as an example
+#' }
+#'
+#' @export
+get_dataset <- function(
+                                          datawarehouse,
+                                          prefix,
+                                          pipeline_handler = "local") {
+  
+  checkmate::assert_choice(datawarehouse$type, c("bucket"))
+
+  if (datawarehouse$type == "bucket") {
+    if (pipeline_handler == "lambda") {
+      s3_client <- paws.storage::s3()
+    } else {
+      s3_client <<- paws.storage::s3(
+        config = list(
+          credentials = list(
+            creds = datawarehouse$credentials
+          ),
+          region = Sys.getenv("AWS_REGION")
+        )
+      )
+    }
+
+    s3_object <- s3_client$get_object(
+      Bucket = datawarehouse$name, 
+      Key = paste(prefix, filename, sep="/")
+    )
+
+    df <- arrow::read_parquet(s3_object$Body)
+
+    return(df)  
   }
 
 }
