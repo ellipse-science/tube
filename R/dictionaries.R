2309c4fa26ce8e4f7ca0ee35271d836cee69628d
p2xcode <p2x@p2xco.de>
2024-01-23 04:25:48 +0000

    Refactor get_dictionary function to use session and table instead of datawarehouse_name and dict_df

--- R/dictionaries.R
+++ R/dictionaries.R
@@ -1,43 +1,13 @@
 #' @export
-get_dictionary <- function(datawarehouse_name, topic, lang = c("en","fr"), credentials) {
-    logger::log_debug("[tube::get_dictionary] entering function")
+get_dictionary <- function(session, topic, lang = c("en","fr")) {
+  logger::log_debug("[pumpr::get_dictionary] entering function")
 
-  # TODO: checkmate parameters validations and error handling
-
-  logger::log_debug("[tube::get_dictionary] instanciating s3 client")
-  s3_client <- paws.storage::s3(
-    config = c(
-      credentials, 
-      close_connection = TRUE)
-  )
-
-  # Get dictionary file from datawarehouse
-  file_key <- paste("dict_", topic, sep = "")
-  file_key <- paste("_dictionaries", "/", file_key, ".csv", sep="")
-
-  object <- s3_client$get_object( 
-    Bucket = datawarehouse_name,
-    Key = file_key
-  )
-
-  raw_to_char <- object$Body %>% rawToChar
-  csv_text <- textConnection(raw_to_char)
-  dict_df <- utils::read.csv2(csv, encoding = "UTF-8")
+  table <- get_datawarehouse_table(session, paste("dict_", topic, sep = ""))
 
   # Filter on language provided in lang if language is a dictionary feature
-  if (!is.null(dict_df$language)) {
-    dict_df <- dict_df[dict_df$language %in% lang, ]
-
-    # Remove language column
-    dict_df$language <- NULL
+  if (!is.null(table$language)) {
+    table <- table[table$language %in% lang, ]
   }
 
-  dict_list <- list()
-  for (c in unique(dict_df$category)) {
-    dict_list[[c]] <- dict_df$item[dict_df$category == c]
-  }
-
-  # Convert dataframe to quanteda dict and return it
-  qdict <- quanteda::dictionary(as.list(dict_list))
-  return(qdict)
+  return(table)
 }
