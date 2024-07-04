b11c3c04c7ea40571b7fe8555e000f403e09ad6c
patoscope <patrick@infoscope.ca>
2024-07-04 19:38:27 +0000

    feat: Add list_s3_partitions function for listing S3 partitions

--- R/s3.R
+++ R/s3.R
@@ -24,3 +24,27 @@ list_s3_buckets <- function(type, credentials) {
   logger::log_debug("[tube::list_s3_buckets] returning results")
   return(bucket_list)
 }
+
+list_s3_partitions <- function(bucket, credentials) {
+  logger::log_debug("[tube::list_s3_partitions] entering function")
+
+  logger::log_debug("[tube::list_s3_partitions] instanciating s3 client")
+  s3_client <- paws.storage::s3(
+    config = c(
+      credentials,
+      close_connection = TRUE)
+  )
+
+  logger::log_debug("[tube::list_s3_partitions] listing partitions")
+  r <- s3_client$list_objects_v2(
+    Bucket = bucket,
+    Delimiter = "/"
+  )
+
+  # Make a unnamed list of it
+  logger::log_debug("[tube::list_s3_partitions] wrangling result")
+  partition_list <- r$CommonPrefixes
+  partition_list <- lapply(partition_list, function(x) x$Prefix)
+  logger::log_debug("[tube::list_s3_partitions] returning results")
+  return(partition_list)
+}
