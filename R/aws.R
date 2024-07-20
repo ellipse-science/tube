b82c96ffa18c3e0648fb8752d3fbeb19e342e0a4
patoscope <patrick@infoscope.ca>
2024-07-20 23:52:47 +0000

    document and improve get_aws_credentials function to support multiple environnement in the same R session to allow to work in DEV and PROD simultaneously

--- R/aws.R
+++ R/aws.R
@@ -1,11 +1,37 @@
-# Checks and returns AWS credentials to be used by other
-# non-exported functions in this package
-#
-get_aws_credentials <- function() {
+#' Returns the AWS credentials in a format that is
+#' compliant with the paws functions calls
+#' 
+#' It is used to get the credentials from the environment variables
+#' set in ~/.Renviron in the following format:
+#' 
+#' #dev
+#' AWS_ACCESS_KEY_ID_DEV=<the access key id for the DEV account>
+#' AWS_SECRET_ACCESS_KEY_DEV=<the secret access key for the DEV account >
+#prod
+#' AWS_ACCESS_KEY_ID_PROD=<the access key id for the PROD account>
+#' AWS_SECRET_ACCESS_KEY_PROD=<the access key id for the PROD account>
+#' 
+#' The function checks if the credentials are valid by trying to list the buckets
+#' in the account. If the credentials are not valid, the function returns NULL
+#' and displays an error message.
+#' 
+#' The value returned must be passed to all functions that use paws functions
+#'
+#' @param env The environnement ("DEV" or "PROD")
+#' @returns a list structure compliant with the paws functions calls containing the AWS credentials
+#' for the specified environment
+get_aws_credentials <- function(env) {
   logger::log_debug("[get_aws_credentials] entering function")
 
-  aws_access_key_id <- Sys.getenv("AWS_ACCESS_KEY_ID")
-  aws_secret_access_key <- Sys.getenv("AWS_SECRET_ACCESS_KEY")
+  # Check if the environment is valid
+  if (!check_env(env)) {
+    cli::cli_alert_danger("Oups, l'environnement que vous avez spécifié n'est pas valide! 😅")
+    logger::log_error("[get_aws_credentials] invalid environment")
+    return(NULL)
+  }  
+
+  aws_access_key_id <- Sys.getenv(paste0("AWS_ACCESS_KEY_ID_", env))
+  aws_secret_access_key <- Sys.getenv(paste0("AWS_SECRET_ACCESS_KEY_", env))
 
   if (aws_access_key_id == "" || aws_secret_access_key == "") {
     usage <-
@@ -41,4 +67,3 @@ get_aws_credentials <- function() {
   logger::log_debug("[get_aws_credentials] successful connection to aws")
   return(creds)
 }
-
