a276528df26928abd89034759f6979cd638229d8
patoscope <patrick@infoscope.ca>
2024-08-21 08:08:01 +0000

    revert to not use profile_name

--- R/ellipse.R
+++ R/ellipse.R
@@ -75,22 +75,21 @@ ellipse_connect <- function(
 
   cli::cli_alert_info("Pour déconnecter: tube::ellipse_disconnect(objet_de_connexion)")
 
-  if (Sys.getenv("_HANDLER") == "lambda_handler") {
-    con <- DBI::dbConnect(noctua::athena(),
-      schema_name = schema_name,
-      profile_name = env,
-      work_group = "ellipse-work-group",
-      s3_staging_dir = paste0("s3://", athena_staging_bucket))
-  } else {
-    cli::cli_alert_info("Connexion en cours...")
-    con <- DBI::dbConnect(noctua::athena(),
-      aws_access_key_id = aws_access_key_id,
-      aws_secret_access_key = aws_secret_access_key,
-      schema_name = schema_name,
-      profile_name = env,
-      work_group = "ellipse-work-group",
-      s3_staging_dir = paste0("s3://", athena_staging_bucket))
-  }
+  # if (Sys.getenv("_HANDLER") == "lambda_handler") {
+  #   con <- DBI::dbConnect(noctua::athena(),
+  #     schema_name = schema_name,
+  #     profile_name = env,
+  #     work_group = "ellipse-work-group",
+  #     s3_staging_dir = paste0("s3://", athena_staging_bucket))
+  # } else {
+  cli::cli_alert_info("Connexion en cours...")
+  con <- DBI::dbConnect(noctua::athena(),
+    aws_access_key_id = aws_access_key_id,
+    aws_secret_access_key = aws_secret_access_key,
+    schema_name = schema_name,
+    work_group = "ellipse-work-group",
+    s3_staging_dir = paste0("s3://", athena_staging_bucket))
+  # }
 
   attr(con, "profile_name") <- env
 
