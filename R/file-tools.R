c10689e100c0efd9af64669faf520e018875876d
patoscope <patrick@infoscope.ca>
2025-08-14 00:15:58 +0000

    fix this pdamn progressbar!

--- R/file-tools.R
+++ R/file-tools.R
@@ -133,7 +133,7 @@ read_file_by_extension <- function(filepath, extension) {
   ext <- tolower(extension)
 
   # Suppress messages from file reading functions
-  df <- suppressWarnings({
+  df <- suppressMessages(suppressWarnings({
     switch(ext,
       "csv" = read_csv_with_overflow_handling(filepath),
       "dta" = haven::read_dta(filepath),
@@ -145,7 +145,7 @@ read_file_by_extension <- function(filepath, extension) {
       "dat" = read_dat_with_overflow_handling(filepath),
       stop("Format de fichier non supporté: ", ext)
     )
-  })
+  }))
 
   # Convert to tibble for consistency
   tibble::as_tibble(df)
